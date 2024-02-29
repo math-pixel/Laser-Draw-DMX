4fdc6e7574d838900aef9edda328eb2ad7f118ff
math-pixel <mathcb74@gmail.com>
2024-02-29 10:05:58 +0100

    Add index of point feedback

--- drawing.js
+++ drawing.js
@@ -1,15 +1,24 @@
 let savedPoint = []
 
+let curentFrame = 0
+let frameRate = 4
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-  
-  for (const currentPoint of savedPoint) {
-    circle(currentPoint[0], currentPoint[1], 20);
-  }
+
+    background(220);
+
+    let index = 0
+    for (let currentPoint of savedPoint) {
+        circle(currentPoint[0], currentPoint[1], 100);
+        text(index, currentPoint[0], currentPoint[1]);
+        // console.log("s")
+        index += 1
+    }
+
 }
 
 function addPoint(x, y){
@@ -18,6 +27,10 @@ function addPoint(x, y){
 
 function mouseDragged() {
   addPoint(mouseX, mouseY)
+}
+
+function mouseRelease(){
+    
 }
 
 function resetCanvas(){
