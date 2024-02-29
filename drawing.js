af06583130f5aea7ce8b901291f151cbeceb7f98
math-pixel <mathcb74@gmail.com>
2024-02-29 09:46:24 +0100

    Create P5 canvas and save drawing

---
+++ drawing.js
@@ -0,0 +1,21 @@
+let savedPoint = []
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for (const currentPoint of savedPoint) {
+    circle(currentPoint[0], currentPoint[1], 20);
+  }
+}
+
+function addPoint(x, y){
+  savedPoint.push([x,y])
+}
+
+function mouseDragged() {
+  addPoint(mouseX, mouseY)
+}
